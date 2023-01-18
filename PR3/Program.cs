aa3f140f2be174c1fee743afb710a3bc34cfdfbb
is13096 <is13096@B303-02>
2023-01-18 15:12:28 +0700

    допустим

--- PR3/Program.cs
+++ PR3/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace PR3
@@ -9,32 +10,27 @@ namespace PR3
     {
         static void Main(string[] args)
         {
+ 
             StudenDifferentiated tom = new StudenDifferentiated();
             StudenDifferentiated alex = new StudenDifferentiated();
             StudenDifferentiated dima = new StudenDifferentiated();
             StudenDifferentiated liza = new StudenDifferentiated();
-
-           string y = Console.ReadLine();
-            if(y == "y")
-            {
-             bool bol = true; 
-            }
-
             string personName = tom.name;
             int semestr = tom.semestr;
             string subject = tom.subject;
             string attestatsi = tom.attestatsi;
             string[] sub = { "Математика", "Русский язык", "Физика" };
             Console.WriteLine($"Студент: {personName}  Симестр: {semestr} Придмет: {subject}  Аттетация: {attestatsi} ");
-            List();
-
-            void List()
+            NOattis();
+            Yesattis();
+            void Yesattis()
             {
                 tom.name = "Tom";
                 tom.semestr = 3;
                 tom.subject = sub[0];
                 tom.attestatsi = "Аттестован";
                 tom.otsenka = 5;
+                tom.Zatet = true;
                 tom.Print();
                 tom.otse();
 
@@ -43,27 +39,36 @@ namespace PR3
                 alex.subject = sub[1];
                 alex.attestatsi = "Аттестован";
                 alex.otsenka = 4;
+                alex.Zatet = true;
                 alex.Print();
                 alex.otse();
-
+            }
+            void NOattis()
+            {
                 dima.name = "Dima";
-                dima.semestr = 1;
+                dima.semestr = 5;
                 dima.subject = sub[2];
                 dima.attestatsi = "НЕ Аттестован";
                 dima.otsenka = 2;
+               dima.Zatet = false;
                 dima.Print();
                 dima.otse();
-               
-                
+
                 liza.name = "liza";
                 liza.semestr = 5;
                 liza.subject = sub[0];
                 liza.attestatsi = "НЕ Аттестован";
                 liza.otsenka = 3;
+                liza.Zatet = true;
                 liza.Print();
                 liza.otse();
             }
+            //if (attestatsi == "НЕ Аттестован")
+            //{
+            //    liza.otse()
+            //}
 
+           
 
         }
 
@@ -73,40 +78,25 @@ namespace PR3
             public int semestr = 1 ;
             public string subject = "Null";
             public string attestatsi = "Null";
-            public bool bol;
+        
             public void Print()
             {
-                if (bol == true)
-                {
-                    if (attestatsi == "НЕ Аттестован")
-                    {
+                    if(attestatsi == "НЕ Аттестован")
                         Console.WriteLine($"Студент: {name}  Симестр: {semestr} Придмет: {subject}  Аттетация: {attestatsi} ");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine($"Студент: {name}  Симестр: {semestr} Придмет: {subject}  Аттетация: {attestatsi} ");
-                }
+                    
             }
-            public void show()
-            {
-                // Console.WriteLine(name.show());
-
-
-            }
-
         }
         class StudenDifferentiated : attestatsii
         {
+            public bool Zatet;
             public int otsenka;
             public void otse()
             {
-                Console.WriteLine($"Студент: {name} Оценка за курсовую: {otsenka}");
+                if(Zatet == true){
+                    Console.WriteLine($"Студент: {name} Зачет: {Zatet} Оценка за курсовую: {otsenka}");
+                }
+               
             }
-
-
-
-
         }
     }
 }
