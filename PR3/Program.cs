06e80a15972eec96d0fabcf4b1f98b9a9e462309
student <student@B206-2>
2023-01-18 11:09:10 +0700

    1-2

--- PR3/Program.cs
+++ PR3/Program.cs
@@ -6,29 +6,41 @@ namespace PR3
     {
         static void Main(string[] args)
         {
-            Student tom = new Student();
-            string StudentName = tom.name;
-            int Semestr = tom.semestr;
+            Student tom = new Student();      
+            Student alex = new Student();
+            Student dima = new Student();
+
+            string personName = tom.name;
+            int semestr = tom.semestr;
             string subject = tom.subject;
             string attestatsi = tom.attestatsi;
+            string[] sub = { "Математика", "Русский язык", "Физика" };
            
+           
+                Console.WriteLine($"Студент: {personName}  Симестр: {semestr} Придмет: {subject}  Аттетация: {attestatsi} ");
+
+            
+            //--------------------------------------// 
+            dima.name = "Dima";
+            dima.semestr = 1;
+            dima.subject = sub[3];
+            dima.attestatsi = "НЕАттестован";
+            dima.Print();
+
             tom.name = "Tom";
-            if (tom.semestr > 9)
-            {
-               
-                tom.semestr = ;
-            }
-            tom.subject = "Математика";
+            tom.semestr = 3 ;
+            tom.subject = sub[0];
             tom.attestatsi = "Аттестован";
             tom.Print();
-
-            Student alex = new Student();
+       
             alex.name = "Alex";
             alex.semestr = 2;
-            alex.subject = "Русский язык";
+            alex.subject = sub[1];
             alex.attestatsi = "Аттестован";
             alex.Print();
 
+           
+          
         }
        
         class Student
@@ -38,9 +50,11 @@ namespace PR3
             public int semestr   ;
             public string subject;
             public string attestatsi;
-                       public void Print()
+            public void Print()
             {
-                Console.WriteLine($"Студент: {name}  Симестр: {semestr} Придмет {subject}  Аттетация: {attestatsi} ");
+               
+                    Console.WriteLine($"Студент: {name}  Симестр: {semestr} Придмет: {subject}  Аттетация: {attestatsi} ");
+                
             }
         }
         
