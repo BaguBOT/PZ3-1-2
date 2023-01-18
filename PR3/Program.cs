b98739664f2db0255d8c96216377e96ea636e342
is13096 <is13096@B303-02>
2023-01-18 14:44:02 +0700

    Дороботка с выбором функции и оценки за курсовую

--- PR3/Program.cs
+++ PR3/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace PR3
 {
@@ -6,60 +9,104 @@ namespace PR3
     {
         static void Main(string[] args)
         {
-            Student tom = new Student();      
-            Student alex = new Student();
-            Student dima = new Student();
-            Student liza = new Student();
+            StudenDifferentiated tom = new StudenDifferentiated();
+            StudenDifferentiated alex = new StudenDifferentiated();
+            StudenDifferentiated dima = new StudenDifferentiated();
+            StudenDifferentiated liza = new StudenDifferentiated();
+
+           string y = Console.ReadLine();
+            if(y == "y")
+            {
+             bool bol = true; 
+            }
 
             string personName = tom.name;
             int semestr = tom.semestr;
             string subject = tom.subject;
             string attestatsi = tom.attestatsi;
-            string[] sub = { "Математика", "Русский язык", "Физика" };   
+            string[] sub = { "Математика", "Русский язык", "Физика" };
             Console.WriteLine($"Студент: {personName}  Симестр: {semestr} Придмет: {subject}  Аттетация: {attestatsi} ");
-          
-            tom.name = "Tom";
-            tom.semestr = 3 ;
-            tom.subject = sub[0];
-            tom.attestatsi = "Аттестован";
-            tom.Print();
-       
-            alex.name = "Alex";
-            alex.semestr = 2;
-            alex.subject = sub[1];
-            alex.attestatsi = "Аттестован";
-            alex.Print();
-          ;
-            dima.name = "Dima";
-            dima.semestr = 1;
-            dima.subject = sub[2];
-            dima.attestatsi = "НЕ Аттестован";
-            dima.Print();
-           
-            liza.name = "liza";
-            liza.semestr = 5;
-            liza.subject = sub[0];
-            liza.attestatsi = "НЕ Аттестован";
-            liza.Print();
+            List();
+
+            void List()
+            {
+                tom.name = "Tom";
+                tom.semestr = 3;
+                tom.subject = sub[0];
+                tom.attestatsi = "Аттестован";
+                tom.otsenka = 5;
+                tom.Print();
+                tom.otse();
+
+                alex.name = "Alex";
+                alex.semestr = 2;
+                alex.subject = sub[1];
+                alex.attestatsi = "Аттестован";
+                alex.otsenka = 4;
+                alex.Print();
+                alex.otse();
+
+                dima.name = "Dima";
+                dima.semestr = 1;
+                dima.subject = sub[2];
+                dima.attestatsi = "НЕ Аттестован";
+                dima.otsenka = 2;
+                dima.Print();
+                dima.otse();
+               
+                
+                liza.name = "liza";
+                liza.semestr = 5;
+                liza.subject = sub[0];
+                liza.attestatsi = "НЕ Аттестован";
+                liza.otsenka = 3;
+                liza.Print();
+                liza.otse();
+            }
+
 
         }
-       
-        class Student
+
+        class attestatsii
         {
-            public string name = "Null" ;
-          
-            public int semestr   ;
-            public string subject;
-            public string attestatsi;
+            public string name = "Null";
+            public int semestr = 1 ;
+            public string subject = "Null";
+            public string attestatsi = "Null";
+            public bool bol;
             public void Print()
             {
-
-                if (attestatsi == "НЕ Аттестован") Console.WriteLine($"Студент: {name}  Симестр: {semestr} Придмет: {subject}  Аттетация: {attestatsi} ");
+                if (bol == true)
+                {
+                    if (attestatsi == "НЕ Аттестован")
+                    {
+                        Console.WriteLine($"Студент: {name}  Симестр: {semestr} Придмет: {subject}  Аттетация: {attestatsi} ");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Студент: {name}  Симестр: {semestr} Придмет: {subject}  Аттетация: {attestatsi} ");
+                }
             }
+            public void show()
+            {
+                // Console.WriteLine(name.show());
+
+
+            }
+
         }
-        
-      
+        class StudenDifferentiated : attestatsii
+        {
+            public int otsenka;
+            public void otse()
+            {
+                Console.WriteLine($"Студент: {name} Оценка за курсовую: {otsenka}");
+            }
 
 
+
+
+        }
     }
 }
