c5da5583d7bf6f2e070cc21619d0f98a2e09bbcf
Александр Индинок <aleksandar_34@mail.ru>
2023-01-19 13:32:24 +0700

    переделпать массив

--- PR3/Program.cs
+++ PR3/Program.cs
@@ -10,92 +10,112 @@ namespace PR3
     {
         static void Main(string[] args)
         {
- 
             StudenDifferentiated tom = new StudenDifferentiated();
             StudenDifferentiated alex = new StudenDifferentiated();
             StudenDifferentiated dima = new StudenDifferentiated();
             StudenDifferentiated liza = new StudenDifferentiated();
-            string personName = tom.name;
-            int semestr = tom.semestr;
-            string subject = tom.subject;
-            string attestatsi = tom.attestatsi;
-            string[] sub = { "Математика", "Русский язык", "Физика" };
+            string personName = tom.Name;
+            int semestr = tom.Semestr;
+            string subject = tom.Subject[4]; 
+            
+            string attestatsi = tom.Attestatsi;
+            //{ "Математика", "Русский язык", "Физика","ОБЖ"} ;
             Console.WriteLine($"Студент: {personName}  Симестр: {semestr} Придмет: {subject}  Аттетация: {attestatsi} ");
             NOattis();
-            Yesattis();
-            void Yesattis()
+            YESattis();
+            void YESattis()
             {
-                tom.name = "Tom";
-                tom.semestr = 3;
-                tom.subject = sub[0];
-                tom.attestatsi = "Аттестован";
-                tom.otsenka = 5;
+                tom.Name = "Tom";
+                tom.Semestr = 3;
+                tom.Subject = sub[0];
+                tom.Attestatsi = "Аттестован";
+                tom.Otsenka = 5;
                 tom.Zatet = true;
                 tom.Print();
                 tom.otse();
-
-                alex.name = "Alex";
-                alex.semestr = 2;
-                alex.subject = sub[1];
-                alex.attestatsi = "Аттестован";
-                alex.otsenka = 4;
+                ////////////////////////////////////////////////
+                alex.Name = "Alex";
+                alex.Semestr = 2;
+                alex.Subject = sub[1];
+                alex.Attestatsi = "Аттестован";
+                alex.Otsenka = 4;
                 alex.Zatet = true;
                 alex.Print();
                 alex.otse();
             }
             void NOattis()
             {
-                dima.name = "Dima";
-                dima.semestr = 5;
-                dima.subject = sub[2];
-                dima.attestatsi = "НЕ Аттестован";
-                dima.otsenka = 2;
-               dima.Zatet = false;
+                dima.Name = "Dima";
+                dima.Semestr =5;
+                dima.Subject = sub[2];
+                dima.Attestatsi = "НЕ Аттестован";
+                dima.Otsenka = 2;
+                dima.Zatet = false;
                 dima.Print();
                 dima.otse();
-
-                liza.name = "liza";
-                liza.semestr = 5;
-                liza.subject = sub[0];
-                liza.attestatsi = "НЕ Аттестован";
-                liza.otsenka = 3;
+                ////////////////////////////////////////////
+                liza.Name = "liza";
+                liza.Semestr = 5;
+                liza.Subject = sub[0];
+                liza.Attestatsi = "НЕ Аттестован";
+                liza.Otsenka = 3;
                 liza.Zatet = true;
                 liza.Print();
                 liza.otse();
             }
-            //if (attestatsi == "НЕ Аттестован")
-            //{
-            //    liza.otse()
-            //}
-
-           
-
         }
-
         class attestatsii
         {
-            public string name = "Null";
-            public int semestr = 1 ;
-            public string subject = "Null";
-            public string attestatsi = "Null";
-        
+            private string _name = "Null";
+            public string Name
+            {
+                get { return _name; }
+                set { _name = value; }  
+            }
+            private int _semestr = 1;
+            public int Semestr
+            {
+                get { return _semestr; }
+                set {
+                   
+                    _semestr = value; }
+            }
+            private string[] _subject;
+            public string[] Subject
+            {
+                get { return _subject; }    
+                set { _subject = value; }
+            }
+            private string _attestatsi = "Null";
+            public string Attestatsi
+            {
+                get { return _attestatsi; }
+                set { _attestatsi = value; }
+            }
             public void Print()
             {
-                    if(attestatsi == "НЕ Аттестован")
-                        Console.WriteLine($"Студент: {name}  Симестр: {semestr} Придмет: {subject}  Аттетация: {attestatsi} ");
-                    
+                if (Attestatsi == "НЕ Аттестован")
+                 Console.WriteLine($"Студент: {Name}  Симестр: {Semestr} Придмет: {Subject}  Аттетация: {Attestatsi} ");
+           
             }
         }
         class StudenDifferentiated : attestatsii
         {
+
             public bool Zatet;
-            public int otsenka;
+
+            private int _otsenka;
+            public int Otsenka
+            {
+                get { return _otsenka; }
+                set { _otsenka = value; }
+            }
             public void otse()
             {
-                if(Zatet == true){
-                    Console.WriteLine($"Студент: {name} Зачет: {Zatet} Оценка за курсовую: {otsenka}");
+                if (Zatet == true)
+                {
+                    Console.WriteLine($"Студент: {Name} Зачет: {Zatet} Оценка за курсовую: {Otsenka}");
                 }
-               
             }
         }
     }
